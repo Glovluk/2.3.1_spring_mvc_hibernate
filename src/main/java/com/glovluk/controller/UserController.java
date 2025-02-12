76d3b5013ad363052cba50f285e00f816648f2b4
Glovluk <79121131077@yandex.ru>
2025-02-12 11:50:02 +0300

    - Забыл про условие: (Используйте только GET/POST маппинги)

--- src/main/java/com/glovluk/controller/UserController.java
+++ src/main/java/com/glovluk/controller/UserController.java
@@ -5,15 +5,13 @@ import com.glovluk.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 public class UserController {
-    
+
     private final UserService userService;
 
     @Autowired
@@ -21,7 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")  // GET-запрос для отображения всех пользователей
     public String showAllUsers(Model model) {
         List<User> allUsers = userService.findAll();
         model.addAttribute("allUsers", allUsers);
@@ -29,7 +27,7 @@ public class UserController {
         return "all-users";
     }
 
-    @RequestMapping("/addNewUser")
+    @GetMapping("/addNewUser")  // GET-запрос для отображения формы добавления нового пользователя
     public String addNewUser(Model model) {
         User employee = new User();
         model.addAttribute("user", employee);
@@ -37,24 +35,25 @@ public class UserController {
         return "users-info";
     }
 
-    @RequestMapping("/saveUser")
+    @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") User user) {
         userService.save(user);
 
         return "redirect:/";
     }
 
-    @RequestMapping("/updateinfo")
+    @GetMapping("/updateinfo")
     public String updateUser(@RequestParam("userId") Long id, Model model) {
         model.addAttribute("user", userService.findById(id));
 
         return "users-info";
     }
 
-    @RequestMapping("/deleteUser")
+    @GetMapping("/deleteUser")
     public String deleteUser(@RequestParam("userId") Long id) {
         userService.delete(id);
 
         return "redirect:/";
     }
 }
+
