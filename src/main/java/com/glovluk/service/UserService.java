b47b8302125c7dbab63cf63e295482024e38f323
Glovluk <79121131077@yandex.ru>
2025-02-14 20:05:20 +0300

    - bcghfdbk pfvtxfybt vtynjhf (Нарушил SOLID. Сервису нужен контракт (интерфейс) + реализация (класс)) (реализовал инъекцию Сервис в Контроллере через абстракцию интерфейс)

--- src/main/java/com/glovluk/service/UserService.java
+++ src/main/java/com/glovluk/service/UserService.java
@@ -1,41 +1,15 @@
 package com.glovluk.service;
 
-
 import com.glovluk.model.User;
-import com.glovluk.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
+public interface UserService {
+    List<User> findAll();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    User findById(Long id);
 
-    @Transactional(readOnly = true)
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
+    void save(User user);
 
-    @Transactional(readOnly = true)
-    public User findById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Entity not found with id: " + id));
-    }
-
-    @Transactional
-    public void save(User user) {
-        userRepository.save(user);
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        userRepository.deleteById(id);
-    }
+    void delete(Long id);
 }
